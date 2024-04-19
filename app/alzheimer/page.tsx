e0e16b3cc46db60b6cf48e081c65fda1eb19ae6f
Rakesh Kanneeswaran <rm1404@srmist.edu.in>
2024-04-19 18:35:01 +0530

    simulated model server

--- app/alzheimer/page.tsx
+++ app/alzheimer/page.tsx
@@ -1,15 +1,47 @@
 "use client"
+import axios from "axios"
 import React from 'react'
 import { useState } from 'react';
 export default function Alzheimer() {
-
-
     const [userSelectedFile, setUserSelectedFile] = useState(null);
+    const tempurl = process.env.GOJORADIXURL;
+    const  ACK = process.env.api_key;
     const [leftImage, setLeftImage] = useState<any>(null);
+
+    // user changes the file 
     function onFileChange(event: any) {
         setUserSelectedFile(event.target.files[0]);
         console.log(userSelectedFile)
     }
+    //on Process 
+    async function onProcess() {
+        if (!leftImage) {
+
+          return;
+        } else {
+
+        console.log("ran on process funciton")
+    
+          const response = await axios({
+            method: "POST",
+            url:process.env.tempurl,
+            params: {
+              api_key:process.env.ACK,
+            },
+            data: leftImage,
+            headers: {
+              "Content-Type": "application/x-www-form-urlencoded",
+            },
+          });
+    
+          if (response.data) {
+            console.log("got response");
+          } 
+        }
+      }
+
+
+    //user uploaded a file
     async function onFileUpload() {
         if (!userSelectedFile) {
             return;
@@ -22,7 +54,6 @@ export default function Alzheimer() {
             base64Data = base64Data.replace(/^data:image\/[a-z]+;base64,/, "");
             setLeftImage(base64Data);
             console.log(base64Data)
-
         };
         reader.readAsDataURL(userSelectedFile);
     }
@@ -37,18 +68,18 @@ export default function Alzheimer() {
                 onChange={onFileChange}
             />
             <button onClick={onFileUpload}>
-                Process
+                Upload file
             </button>
-
+             <button onClick={onProcess}>
+                Processing file
+            </button>
             <div>
                 <div className="bg-slate-100 flex justify-center items-center py-5 h-full border-black border-4 rounded-lg">
-
                     <img
                         className="mt-8 w-min h-96"
                         src={`data:image/jpeg;base64,${leftImage}`}
                         alt="Uploaded"
                     />
-
                 </div>
             </div>
         </div>
