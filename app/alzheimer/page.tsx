9f07e661285c5a125febadf6a2abdb85267ef152
Rakesh Kanneeswaran <rm1404@srmist.edu.in>
2024-04-19 17:55:48 +0530

    converted uploaded image in to base64 url

--- app/alzheimer/page.tsx
+++ app/alzheimer/page.tsx
@@ -7,17 +7,36 @@ export default function Alzheimer() {
     const [userSelectedFile, setUserSelectedFile] = useState(null);
     function onFileChange(event: any) {
         setUserSelectedFile(event.target.files[0]);
-
         console.log(userSelectedFile)
-      }
-  return (
-    <div>
-        <h1>image input box</h1>
-        <input
-          className="bg-slate-950 text-white w-30 px-4 py-2 rounded mx-4"
-          type="file"
-          onChange={onFileChange}
-        />
-    </div>
-  )
+    }
+    async function onFileUpload() {
+        if (!userSelectedFile) {
+            return;
+        }
+        console.log("File upload started");
+        const reader = new FileReader();
+        reader.onload = function () {
+            let base64Data = reader.result
+            // @ts-ignore
+            base64Data = base64Data.replace(/^data:image\/[a-z]+;base64,/, "");
+            console.log(base64Data)
+
+        };
+        reader.readAsDataURL(userSelectedFile);
+    }
+
+
+    return (
+        <div>
+            <h1>image input box</h1>
+            <input
+                className="bg-slate-950 text-white w-30 px-4 py-2 rounded mx-4"
+                type="file"
+                onChange={onFileChange}
+            />
+            <button onClick={onFileUpload}>
+                Process
+            </button>
+        </div>
+    )
 }
